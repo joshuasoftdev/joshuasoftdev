ba1987cac23dac777eb751a7f8c61847e2b2f9eb
joshuasoftdev <123251561+joshuasoftdev@users.noreply.github.com>
2023-10-20 22:56:09 -0700

    api

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ let DATA = {
 
 async function setWeatherInformation() {
   await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=Sacramento&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
+    `api.openweathermap.org/data/2.5/weather?q=Sacramento&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
   )
     .then((r) => r.json())
     .then((r) => {
